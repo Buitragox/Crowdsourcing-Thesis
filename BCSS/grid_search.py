e3b9d90b6003aa9f117192a68276b34795fa968d
Jhoan Buitrago <jhoanuitrago@gmail.com>
2024-01-09 20:24:06 -0500

    Add checkpoints to grid_search

--- BCSS/grid_search.py
+++ BCSS/grid_search.py
@@ -2,6 +2,26 @@ from itertools import product
 import numpy as np
 import json
 
+def save_to_json(exp_histories: list[dict], exp_reports: list[dict], 
+                 history_path: str, report_path: str):
+    """Save results from grid_search to json"""
+    with open(history_path, 'w') as fp:
+        json.dump(exp_histories, fp)
+
+    with open(report_path, 'w') as fp:
+        json.dump(exp_reports, fp)
+
+
+def load_json(history_path: str, report_path: str) -> tuple[dict, dict]:
+    """Save results from grid_search to json"""
+    with open(history_path, 'r') as fp:
+        exp_history = json.load(fp)
+
+    with open(report_path, 'r') as fp:
+        exp_report = json.load(fp)
+
+    return exp_history, exp_report
+
 
 def run_train_test(X_train, labels, X_test, Y_test, build_func, 
                    evaluate_func, epochs=25, batch_size=8, validation_split=0.0, 
@@ -37,13 +57,16 @@ def repeat_train_test(X_train, labels, X_test, Y_test, build_func,
 
 def grid_search(X_train, labels, X_test, Y_test, build_func, 
                 evaluate_func, repeat=10, epochs=25, batch_size=8, 
-                validation_split=0.0, **build_kwargs):
+                validation_split=0.0, report_path="", history_path="", **build_kwargs):
     """
     Train and test the model for each combination of build parameters "repeat" amount of times. 
     """
     key_args = build_kwargs.keys()
     exp_histories = [] #histories of each experiment
     exp_reports = [] #reports of each experiment
+
+    if report_path != "" and history_path != "":
+        exp_histories, exp_reports = load_json(history_path, report_path)
 
     if len(key_args) == 0:
         run_history, run_report = repeat_train_test(X_train, labels, X_test, Y_test,
@@ -55,6 +78,14 @@ def grid_search(X_train, labels, X_test, Y_test, build_func,
 
     else:
         args_combinations = product(*build_kwargs.values())
+        done = set()
+        if len(exp_reports) != 0:
+            for exp in exp_reports:
+                args = tuple(exp['args'].values())
+                done.add(args)
+            print("Finished experiments:", done)
+            args_combinations = list(set(args_combinations) - done)
+        
 
         for args in args_combinations:
             kwargs = {k:v for k, v in zip(key_args, args)}
@@ -67,6 +98,10 @@ def grid_search(X_train, labels, X_test, Y_test, build_func,
 
             exp_histories.append({"args": kwargs, "histories": run_history})
             exp_reports.append({"args": kwargs, "reports": run_report})
+
+            save_to_json(exp_histories, exp_reports, history_path, report_path)
+
+
 
     return exp_histories, exp_reports
 
@@ -102,12 +137,3 @@ def show_results(exp_histories, exp_reports, K):
         print(f'\tstd f1 scores: {std_f1_scores}')
         print(f'\tmean accuracy: {mean_accuracy}')
         print(f'\tstd accuracy: {std_accuracy}')
-
-
-def save_to_json(path, exp_histories, exp_reports, history_name, report_name):
-    """Save results from grid_search to json"""
-    with open(f'{path}/{history_name}', 'w') as fp:
-        json.dump(exp_histories, fp)
-
-    with open(f'{path}/{report_name}', 'w') as fp:
-        json.dump(exp_reports, fp)
